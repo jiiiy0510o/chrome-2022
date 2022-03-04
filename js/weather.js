45df11258eafd538030a0c52fb39b1fa792f1b9d
Young <jiiiy0510o@gmail.com>
2022-03-04 18:02:30 +0900

    to-do, weather update

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const API_KEY ="257f4e6c8fc561a25d61cb3c90b41002";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json()
+        .then(data=>{
+            const city = document.querySelector("#weather span:first-child");
+            const weather = document.querySelector("#weather span:last-child");
+            city.innerText = `@${data.name}`;
+            weather.innerText = `${data.weather[0].main} ${data.main.temp}`;
+        }));
+}   
+function onGeoError() {
+    alert("Can't find you. No weather for you");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
